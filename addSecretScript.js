0580a38c8864c8988c08e3781b795ca47e17d0d6
Patrick <prbaldwin@oakland.edu>
2020-04-09 01:30:05 -0400

    added firebase

---
+++ addSecretScript.js
@@ -0,0 +1,59 @@
+let newSecretTextToAdd = "";
+var firebaseConfig = {
+    apiKey: "AIzaSyCnW60Q61rA-FhQ30NtJKVq5lUCV9BTwfg",
+    authDomain: "secret-a5756.firebaseapp.com",
+    databaseURL: "https://secret-a5756.firebaseio.com",
+    projectId: "secret-a5756",
+    storageBucket: "secret-a5756.appspot.com",
+    messagingSenderId: "586926212316",
+    appId: "1:586926212316:web:73a916afa2c26ae8632019"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+let alreadySent = true;
+
+$(document).ready(function () {
+    getAllSecrets()
+});
+
+function getAllSecrets() {
+    return new Promise((resolve, reject) => {
+        var secretsRef = database.ref(`/Secrets/`);
+        secretsRef.on("value", function (snapshot) {
+            const secrets = snapshot.val();
+            console.log(secrets);
+
+            if (secrets && !alreadySent) {
+                addSecret(newSecretTextToAdd, secrets.length);
+            } else if (!alreadySent) {
+                addSecret(newSecretTextToAdd, 1);
+            }
+
+            resolve(secrets);
+        });
+    });
+}
+
+function addSecret(secret, index) {
+    alreadySent = true;
+    const date = new Date();
+    firebase.database().ref('Secrets/' + index).set({
+        secret: secret,
+        timestamp: date.toString()
+    });
+}
+
+
+function saveNewSecretClicked() {
+    alreadySent = false;
+    newSecretTextToAdd = $("#secretInput").val();
+    console.log(newSecretTextToAdd);
+    getAllSecrets();
+};
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -11,6 +11,19 @@ var iTextPos = 0; // initialise text position
 var sContents = ''; // initialise contents variable
 var iRow; // initialise current row
 
+var firebaseConfig = {
+    apiKey: "AIzaSyCnW60Q61rA-FhQ30NtJKVq5lUCV9BTwfg",
+    authDomain: "secret-a5756.firebaseapp.com",
+    databaseURL: "https://secret-a5756.firebaseio.com",
+    projectId: "secret-a5756",
+    storageBucket: "secret-a5756.appspot.com",
+    messagingSenderId: "586926212316",
+    appId: "1:586926212316:web:73a916afa2c26ae8632019"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
 function typewriter() {
     document.getElementById('scroll').scrollIntoView();
     sContents = ' ';
@@ -34,19 +47,55 @@ function typewriter() {
 }
 
 $(document).ready(function () {
-    aText.push("Dear Liv,");
-    aText.push("I know that life is tough right now.");
-    aText.push("I know that you weren't exactly built for quarantine.");
-    aText.push("But I also know that you are trying so hard to make everything okay.");
-    aText.push("And I promise that soon, everything actually will be.");
-    aText.push("But for now, it's okay to not have it all together.");
-    aText.push("I love you so much, I see how hard you are trying, and I know that this too shall pass.");
-    aText.push("-Pat");
+    // aText.push("Dear Liv,");
+    // aText.push("I know that life is tough right now.");
+    // aText.push("I know that you weren't exactly built for quarantine.");
+    // aText.push("But I also know that you are trying so hard to make everything okay.");
+    // aText.push("And I promise that soon, everything actually will be.");
+    // aText.push("But for now, it's okay to not have it all together.");
+    // aText.push("I love you so much, I see how hard you are trying, and I know that this too shall pass.");
+    // aText.push("-Pat");
+
+    getAllSecrets()
+});
+
+
+
+function getAllSecrets() {
+    return new Promise((resolve, reject) => {
+        var secretsRef = database.ref(`/Secrets/`);
+        secretsRef.on("value", function (snapshot) {
+            const secrets = snapshot.val();
+            console.log(secrets);
+            getLatestSecret(secrets);
+            $("#secretCount").text(secrets.length - 1);
+            resolve(secrets);
+        });
+    });
+}
+
+function getLatestSecret(secrets) {
+    const latestSecret = secrets[secrets.length - 1].secret;
+    console.log("latest secret is " + latestSecret);
+    splitStringIntoArray(latestSecret)
+}
+
+function splitStringIntoArray(string) {
+    string = addStringWrapper(string);
+    const secretStringArr = string.split(". ");
+    for (var i = 1; i < secretStringArr.length - 2; i++) {
+        secretStringArr[i] = secretStringArr[i] + ".";
+    }
+    console.log(secretStringArr);
+    aText = secretStringArr;
     iArrLength = aText[0].length;
     typewriter()
+}
+
+function addStringWrapper(string) {
+    return "Dear Liv,. " + string + " -Pat. "
+}
 
 
 
-
-});
 
